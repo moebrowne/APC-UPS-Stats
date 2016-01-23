22184b3c0860ba2b303fe675cefd227af0761557
MoeBrowne <oliver.browne@uplyme.com>
2016-01-23 22:31:23 +0000

    Added a hash check to ensure the status file has actually changed

--- watch.sh
+++ watch.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+. ./libs/md5sum.sh
+
 #STATUS_FILE=/var/log/apcupsd.status
 STATUS_FILE=./apcupsd.status
+STATUS_FILE_HASH=$(hashFile "$STATUS_FILE")
 
 inotifywait -e modify -m "$STATUS_FILE" | while read data; do
-  echo "$data"
+
+  # Check if the file actually changed contents
+  HASH=$(hashFile "$STATUS_FILE")
+  if [ "$HASH" = "$STATUS_FILE_HASH" ]; then
+    continue;
+  fi
+
+  STATUS_FILE_HASH="$HASH"
+
+  echo "$data $HASH"
 done

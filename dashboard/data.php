ac7d00f2be2f4e6804474fb303802c7ebe3d6a20
MoeBrowne <oliver.browne@uplyme.com>
2016-01-24 22:18:23 +0000

    Added a data feed from the db to populate the graph on load

---
+++ dashboard/data.php
@@ -0,0 +1,35 @@
+<?php
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=APCUPS', 'APCUPSStats', 'bm6+h7%w_gn!4');
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$data = [];
+$labels = [];
+
+foreach($dbh->query('SELECT DATE,LOADPCT from stats') as $row) {
+    $data[] = $row['LOADPCT'];
+    $labels[] = date('Hi', $row['DATE']);
+}
+
+
+$chartData = [
+    'labels' => $labels,
+    'datasets' => [
+        [
+            'fillColor' => "rgba(220,220,220,0.2)",
+            'strokeColor' => "rgba(220,220,220,1)",
+            'pointColor' => "rgba(220,220,220,1)",
+            'pointStrokeColor' => "#fff",
+            'pointHighlightFill' => "#fff",
+            'pointHighlightStroke' => "rgba(220,220,220,1)",
+            'data' => $data
+            ]
+    ]
+];
+
+echo json_encode($chartData);
+

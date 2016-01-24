b3163ce91022f860ffb8fd5c865fb17697af586b
MoeBrowne <oliver.browne@uplyme.com>
2016-01-24 01:31:19 +0000

    Added a file watcher to the stat JSON

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,7 +1,15 @@
 'use strict';
 
 var io = require('socket.io')(3000);
+var fs = require('fs');
 
 io.on('connection', function(socket){
 	console.log('WS: user connected');
+});
+
+fs.watchFile('../apcupsd.status.json', function(curr, prev) {
+	var JSONData = fs.readFileSync('../apcupsd.status.json', { flags: '+w' }).toString();
+	JSONData = (JSONData !== '') ? JSON.parse(JSONData):[];
+	io.emit('data', JSONData);
+	console.log('file changed');
 });
